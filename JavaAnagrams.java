9bf6a9048f8d98cc9e57104deda6c55c3c70af21
Sukesh03 <sukesh.pamujula@gmail.com>
2025-05-22 17:55:09 +0530

    HackerRank Easy Problems

---
+++ JavaAnagrams.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class JavaAnagrams {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        if (a.length() != b.length()) {
+            return false;
+        }
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        int res = 0;
+        for (int i = 0; i < a.length(); i++) {
+            res ^= a.charAt(i);
+            res ^= b.charAt(i);
+        }
+        return res == 0;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
