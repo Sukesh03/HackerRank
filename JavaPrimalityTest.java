2ed6d64d034ffd59d7a1f67631c6ced806fff89b
Sukesh03 <sukesh.pamujula@gmail.com>
2025-06-04 16:58:16 +0530

    HackerRank Easy Problems

---
+++ JavaPrimalityTest.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.*;
+
+
+public class JavaPrimalityTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger bi = new BigInteger(n);
+        System.out.println(bi.isProbablePrime(10) ? "prime" : "not prime");
+
+        bufferedReader.close();
+    }
+}
